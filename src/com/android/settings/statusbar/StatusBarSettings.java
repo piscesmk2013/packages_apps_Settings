c065e219ceba49ae86d3bd08c2810119a5ba051b
spezi77 <spezi7713@gmx.net>
2022-10-19 22:38:53 +0200

    Settings: Add empty status bar fragment to System Dashboard.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ src/com/android/settings/statusbar/StatusBarSettings.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022 The PixelDust Project
+ * Copyright (C) 2022 The PixelExperience
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.android.settings.statusbar;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+@SearchIndexable
+public class StatusBarSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.status_bar_settings);
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.status_bar_settings);
+}
